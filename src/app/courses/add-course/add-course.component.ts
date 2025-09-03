a6d0d2259bcd6b97adc0d39ff105ff17eba63c91
ismaelyoussef1009 <soumayabya73@gmail.com>
2025-09-03 22:10:10 +0100

    adding error handling in the form

--- src/app/courses/add-course/add-course.component.ts
+++ src/app/courses/add-course/add-course.component.ts
@@ -18,7 +18,7 @@ export class AddCourseComponent implements OnInit {
       title: new FormControl(null, [
         Validators.required,
         Validators.minLength(6),
-        Validators.maxLength(100),
+        Validators.maxLength(20),
       ]),
 
       description: new FormControl(null, [
@@ -54,5 +54,51 @@ export class AddCourseComponent implements OnInit {
     this.store.dispatch(addCourse({ course: this.courseForm.value }));
     this.hideCreateForm();
     this.courseForm.reset();
+  }
+
+  showTitleValidationErrors() {
+    const titleControl = this.courseForm.get('title');
+
+    if (titleControl.touched && !titleControl.valid) {
+      if (titleControl.errors['required']) {
+        return 'Title is required.';
+      }
+
+      if (titleControl.errors['minlength']) {
+        return 'Title must be at least 6 characters long.';
+      }
+
+      if (titleControl.errors['maxlength']) {
+        return 'Title can not be 20 characters long.';
+      }
+    }
+    return '';
+  }
+
+  showDescriptionValidationErrors() {
+    const descriptionControl = this.courseForm.get('description');
+    if (descriptionControl.touched && !descriptionControl.valid) {
+      if (descriptionControl.errors['required']) {
+        return 'Description is required.';
+      }
+
+      if (descriptionControl.errors['minlength']) {
+        return 'Description must be at least 10 characters long.';
+      }
+      if (descriptionControl.errors['maxlength']) {
+        return 'Description can not be 5000 characters long.';
+      }
+    }
+    return '';
+  }
+
+  showAuthorValidationErrors() {
+    const authorControl = this.courseForm.get('author');
+    if (authorControl.touched && !authorControl.valid) {
+      if (authorControl.errors['required']) {
+        return 'Author is required.';
+      }
+    }
+    return '';
   }
 }
